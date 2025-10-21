f57d4bb8267334d9c3cc833a3369e1214fde32ca
amandeep-saluja <amandeepsaluja25@gmail.com>
2025-10-21 23:44:53 +0530

    Day 5: Added permissions to hit Health check api

--- backend/src/main/java/com/smarttask/backend/security/SecurityConfig.java
+++ backend/src/main/java/com/smarttask/backend/security/SecurityConfig.java
@@ -44,6 +44,7 @@ public class SecurityConfig {
                 }))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/auth/**").permitAll()
+                        .requestMatchers("/health").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
